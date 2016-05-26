a058bbc7d2aeb8afac990830adcf5a3b6d8c28db
Keith Wood <keith.wood@fairfaxmedia.com.au>
2016-05-26 15:34:36 +1000

    Add to Google calendar

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(function(){
   });
   $('#today,#weekend').click(function() {
     addScrollPos();
+	addToCalendar();
     $('#overlay,#tlwr').hide();
   });
 
@@ -34,5 +35,18 @@ $(function(){
 	};
     localStorage.setItem('myLinks', JSON.stringify(links));
     console.log('save ' + savedPos);
+  }
+  
+  function addToCalendar() {
+    var lastCal = $.parseJSON(localStorage.getItem('lastCal'));
+	if (lastCal && new Date(lastCal.date) > new Date()) {
+      return;
+    }
+	var tomorrow = new Date();
+	tomorrow.setDate(tomorrow.getDate() + 1);
+	tomorrow.setHours(0, 0, 0, 0);
+    localStorage.setItem('lastCal', JSON.stringify({date: tomorrow}));	
+    var gcal = 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20160527T110000Z/20160527T170000Z&location=Anywhere&text=My+Reading&details=Read+these+articles+later.%0A%0A--%0Ahttp://www.thevocal.com.au/MyReading.html';
+	window.open(gcal);
   }
 });
